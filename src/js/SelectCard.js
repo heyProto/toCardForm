6f5e8b97c4494c082b2df96b860ed2fbc27f542a
Dhara Shah <DharaShah@Dharas-MacBook-Air.local>
2017-06-23 19:32:29 +0530

    changed the ui of card creation

---
+++ src/js/SelectCard.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class SelectCard extends Component {
+  render() {
+    return (
+      <div className="single-element" onClick={this.props.onSelectCardClick}>
+        <div className="card-type-icon">
+          <img src={this.props.card.icon_url} />
+        </div>
+        <div className="card-type-name">{this.props.card.name}</div>
+      </div>
+    );
+  }
+}
+
+export default SelectCard;
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  entry: './main.js',
+  output: {
+    path: './',
+    filename: './dist/bundle.min.js',
+  },
+  node: {
+    net: 'empty',
+    tls: 'empty',
+    fs: 'empty'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query:
+        {
+          presets:['react']
+        }
+      },
+      {
+        test: /\.scss$/,
+        loaders: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      }
+    ]
+  }
+};
