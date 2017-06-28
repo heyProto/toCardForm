ad5f8ad32334ea976802db6427fd0e17352484e2
mohit joshi <mojo@pykih.com>
2017-06-28 17:02:55 +0530

    Move slideshow to a new component

--- src/js/ConfirmCard.js
+++ src/js/ConfirmCard.js
@@ -1,30 +1,9 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Slideshow from '../js/Slideshow';
 
 class ConfirmCard extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedCard: 1
-    }
-    this.handleSelectCardClick = this.handleSelectCardClick.bind(this);
-  }
-
-  handleSelectCardClick(event) {
-    console.log("target", event.target);
-    let selectedCard = event.target.getAttribute("data-index");
-    this.setState({
-      selectedCard: selectedCard
-    });
-  }
-
   render() {
-    console.log("render");
-    let selectedCard = this.state.selectedCard,
-      mainImage = 'https://s3.ap-south-1.amazonaws.com/protos.dev/Assets/preview-' + selectedCard + '.png',
-      styles = {
-        opacity: 1
-      };
     return(
       <div id="view_area" className="selected-card-preview">
         <div className="preview-header">
@@ -45,28 +24,7 @@ class ConfirmCard extends Component {
         <div className="preview-description">{this.props.card.description}</div>
         <button type="button" className="default-button primary-button preview-button" onClick={this.props.onSelectConfirmClick.bind(this, this.props.card)}>Continue</button>
         <div className="clearfix"></div>
-        <div className="preview-slideshow">
-          <div className="preview-main">
-            <img className="preview-img" src={mainImage} />
-          </div>
-          <div className="preview-minimap">
-            <div className="preview-mini-div" onClick={this.handleSelectCardClick}>
-              <img className="preview-mini-img" data-index="1" style={selectedCard == 1 ? styles : {}} src="https://s3.ap-south-1.amazonaws.com/protos.dev/Assets/preview-1.png" />
-            </div>
-            <div className="preview-mini-div" onClick={this.handleSelectCardClick}>
-              <img className="preview-mini-img" data-index="2" style={selectedCard == 2 ? styles : {}} src="https://s3.ap-south-1.amazonaws.com/protos.dev/Assets/preview-2.png" />
-            </div>
-            <div className="preview-mini-div" onClick={this.handleSelectCardClick}>
-              <img className="preview-mini-img" data-index="3" style={selectedCard == 3 ? styles : {}} src="https://s3.ap-south-1.amazonaws.com/protos.dev/Assets/preview-3.png" />
-            </div>
-            <div className="preview-mini-div" onClick={this.handleSelectCardClick}>
-              <img className="preview-mini-img" data-index="4" style={selectedCard == 4 ? styles : {}} src="https://s3.ap-south-1.amazonaws.com/protos.dev/Assets/preview-4.png" />
-            </div>
-            <div className="preview-mini-div" onClick={this.handleSelectCardClick}>
-              <img className="preview-mini-img" data-index="5" style={selectedCard == 5 ? styles : {}} src="https://s3.ap-south-1.amazonaws.com/protos.dev/Assets/preview-5.png" />
-            </div>
-          </div>
-        </div>
+        <Slideshow />
       </div>
     )
   }
